7fdbe571679d4c042f14ff940fe687b774b5a16f
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2014-09-22 17:49:36 +0100

    fixed key value for oauth n other tweeking.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -5,65 +5,65 @@ var twitControllers = {};
 
 // define twitController
 twitControllers.twitController = function($scope, $q, twitFactory)
+{
+	// array of tweets
+	$scope.tweets;
+
+	// initialise twitreader factories
+	twitFactory.initialize();
+
+	// uses OAuth to get the latest tweets from the user
+	$scope.refreshTweets = function()
 	{
-		// array of tweets
-		$scope.tweets = [];
-
-		// initialise twitreader factories
-		twitFactory.initialize();
-
-		// uses OAuth to get the latest tweets from the user
-		$scope.refreshTweets = function()
+		twitFactory.getLatestTweets().then(function(data)
 		{
-			twitFactory.getLatestTweets().then(function(data)
+			$scope.tweets = data;
+		});
+	}
+
+	// once the user clicks connect, a popup would show up
+	// to ask authorisation to access personal data
+	// this must use the methods defined in the twitFactory
+	$scope.logIn = function()
+	{
+		twitFactory.connectTwitter().then(function()
+		{
+			// if the authentification is successful,
+			// we would like to hide the log in button...
+			if(twitFactory.isReady())
 			{
-				$scope.tweets = data;
-			});
-		}
-
-		// once the user clicks connect, a popup would show up
-		// to ask authorisation to access personal data
-		// this must use the methods defined in the twitFactory
-		$scope.logIn = function()
-		{
-			twitFactory.connectTwitter().then(function()
-			{
-				// if the authentification is successful,
-				// we would like to hide the log in button...
-				if(twitFactory.isReady())
+				$('#logintButton').fadeOut(function() 
 				{
-					$('#logintButton').fadeOut(function() 
-					{
-						// ... and show the other two.
-						$('#refreshButton, #logoutButton').fadeIn();
-					});
-				}
-			});
-		}
-
-		// clears the OAuth's cache, the user will have to
-		// reauthenticate if he wants to access his timeline
-		// again.
-		$scope.logOut = function()
-		{
-			twitFactory.clearCache();
-			$scope.tweets = [];
-
-			// to logout, we hide refresh/logout buttons...
-			$('#refreshButton', '#logoutButton').fadeOut(function()
-				{
-					$('#loginButton').fadeIn();
+					$('#refreshButton, #logoutButton').fadeIn();
+					$scope.refreshTweets()
 				});
-		}
+			}
+		});
+	}
 
-		// using jQuery we will make sure to hide the 'connect' button
-		// once the user is already connected, and vice-versa.
-		if(twitFactory.isReady())
+	// clears the OAuth's cache, the user will have to
+	// reauthenticate if he wants to access his timeline
+	// again.
+	$scope.logOut = function()
+	{
+		twitFactory.clearCache();
+		$scope.tweets.length = 0;
+
+		// to logout, we hide refresh/logout buttons...
+		$('#refreshButton', '#logoutButton').fadeOut(function() 
 		{
-			$('#loginButton').hide();
-			$('#refreshButton', '#logoutButton').show();
-			$scope.refreshTweets();
-		}
-	};
+			$('#loginButton').fadeIn();
+		});			
+	}
+
+	// using jQuery we will make sure to hide the 'connect' button
+	// once the user is already connected, and vice-versa.
+	if(twitFactory.isReady())
+	{
+		$('#loginButton').hide();
+		$('#refreshButton', '#logoutButton').show();
+		$scope.refreshTweets();
+	}
+};
 
 twitReader.controller(twitControllers);
--- scripts/services.js
+++ scripts/services.js
@@ -3,25 +3,24 @@
 angular.module('twitReader.factories', [])
 	   .factory('twitFactory', function($q)
 	   {
-	   		var PUBLIC_KEY_OAUTH = 'SbQIspWCFyZymN7beGCmPVsnr';
+	   		var PUBLIC_KEY = 'tB5T5hvlnp-QfKMKSsskGpTzNQE';
+	   		var authentificationResult = false;
 
-	   		var authentificationResult = false;   		
+
 	   		var factory =
 	   		{
-		   		initialize : function()
-	   			{
+		   		initialize : function() 
+		   		{
 	   				// initialize OAuth with public key of the app
 	   				// public key was generated through twitter: https://apps.twitter.com/
-	   				OAuth.initialize(PUBLIC_KEY_OAUTH, {cache: true});
+	   				OAuth.initialize(PUBLIC_KEY, {cache: true});
 	   				// try to create an authorisation result when the page loads
 	  				authentificationResult = OAuth.create('twitter');
 	   			},
-
 		   		isReady : function()
 		   		{
 		   			return authentificationResult;
 		   		},
-
 		   		connectTwitter : function()
 		   		{
 		   			// First we create a work of unit using a deferred object.
@@ -35,33 +34,31 @@ angular.module('twitReader.factories', [])
 	   				// or a callback method that should be called once the 
 	   				// promise resolves, rejects or gives a progress update.
 	   				OAuth.popup('twitter', {cache: true}, function(error, result)
+	   				{
+	   					//cache means to execute the callback if the tokens are already present
+	   					if(!error)
 	   					{
-	   						//cache means to execute the callback if the tokens are already present
-	   						if(!error)
-	   						{
-	   							authentificationResult = result;
-	   							deferred.resolve(result);
+	   						authentificationResult = result;
+	   						deferred.resolve();
 
-	   							$('#statusMessage').addClass('text-success');
-	   							$('#statusMessage').text("Success!")
-	   						}
-	   						else
-	   						{
-	   							$('#statusMessage').addClass('text-danger');
-	   							$('#statusMessage').text("Authetification failed!")
-	   						}
-	   					});	
+	   						$('div.status').addClass("text-success");
+	   						$('#statusMessage').text("Successful!");
+	   					}
+	   					else
+	   					{
+	   						$('div.status').addClass("text-danger");
+	   						$('#statusMessage').text("Authentification failed!");
+	   					}
+	   				});	
 
 	   				// We grab and return the promise from the deferred object.
 	   				return deferred.promise;
 	   			},
-
 		   		clearCache : function()
 		   		{
 		   			OAuth.clearCache('twitter');
 		   			authentificationResult = false;
 		  		},
-
 				getLatestTweets : function()
 				{
 					// Create a unit of work of a deferred object using $q
@@ -69,14 +66,15 @@ angular.module('twitReader.factories', [])
 		  		
 		  			// Make a promise out of the deferred object
 		   			var promise = authentificationResult.get('/1.1/statuses/home_timeline.json')
-		   									 .done(function(data)
-		  									 	{
-		  									 		// Once the data is treated and retrieve thanks to Twitter's REST API
-		  									 		// We would like to resolve the deferred object
-		   									 		deferred.resolve(data);
-		   									 	});
-		   				// Return the promise from the deferred object.
-		   				return deferred.promise;
+		   									 			.done(function(data)
+		  									 			{
+		  									 				// Once the data is treated and retrieve thanks to Twitter's REST API
+		  									 				// We would like to resolve the deferred object
+		   									 				deferred.resolve(data);
+		   									 			});
+		   			
+		   			// Return the promise from the deferred object.
+		   			return deferred.promise;
 		   		}
 		   	}
 
